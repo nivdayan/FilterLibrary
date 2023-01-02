f61d9a943125a9b86c468a510c56e8d92026e72e
“Niv <nivdayan@gmail.com>
2023-01-02 12:37:20 +0100

    testing another BF implementation

--- filters/BloomFilter.java
+++ filters/BloomFilter.java
@@ -41,14 +41,25 @@ public class BloomFilter extends Filter {
 		return Math.abs(this_hash % num_bits);
 	}
 	
+	long get_target_bit2(long large_hash, int hash_num) {
+		long this_hash = HashFunctions.xxhash(large_hash, hash_num);
+		long mask = num_bits - 1;
+		long target_bit = this_hash & mask;
+		return target_bit;
+	}
+	
 	@Override
 	protected boolean _insert(long large_hash, boolean insert_only_if_no_match) {
 		
-		long target_bit = Math.abs(large_hash % num_bits);
+		//long target_bit = Math.abs(large_hash % num_bits);
+		
+		long mask = num_bits - 1;
+		long target_bit = large_hash & mask;
+		
 		filter.set(target_bit, true);
 		
 		for (int i = 1; i < num_hash_functions; i++) {
-			target_bit = get_target_bit(large_hash, i);
+			target_bit = get_target_bit2(large_hash, i);
 			//System.out.println(target_bit);
 			filter.set(target_bit, true);
 		}
@@ -56,16 +67,51 @@ public class BloomFilter extends Filter {
 		return true;
 	}
 
+	/*static long reduce(int x, int N) {
+		long res = ((long) x * (long) N) >> 32;
+		return res;
+	}*/
+	
 	@Override
 	protected boolean _search(long large_hash) {
 		
-		long target_bit = Math.abs(large_hash % num_bits);
+		//long target_bit1 = reduce((int)large_hash, (int)num_bits);
+		//System.out.println(target_bit1);
+		
+		//long power = (long)(Math.log(num_bits) / Math.log(2));
+
+		/*QuotientFilter.print_long_in_binary(large_hash, 64);
+		System.out.println();
+		QuotientFilter.print_long_in_binary(num_bits, 64);
+		System.out.println();		
+		QuotientFilter.print_long_in_binary(mask, 64);
+		System.out.println();
+		//QuotientFilter.print_long_in_binary(power, 64);
+		//System.out.println();
+		QuotientFilter.print_long_in_binary(slot, 64);
+		System.out.println();*/
+		
+		//large_hash = large_hash | 1;
+		/*System.out.println();
+		QuotientFilter.print_long_in_binary(-2, 64);
+		QuotientFilter.print_long_in_binary(2, 64);
+		QuotientFilter.print_long_in_binary(large_hash, 64);*/
+		//large_hash = Math.abs(large_hash);
+		//System.out.println();
+		//QuotientFilter.print_long_in_binary(large_hash, 64);
+		long mask = num_bits - 1;
+		long target_bit = large_hash & mask;
+		//QuotientFilter.print_long_in_binary(target_bit, 64);
+		
+		long target_bit2 = Math.abs(large_hash % num_bits);
+		//QuotientFilter.print_long_in_binary(target_bit2, 64);
+		
 		if (! filter.get(target_bit) ) {
 			return false;
 		}
 		
 		for (int i = 1; i < num_hash_functions; i++) {
-			target_bit = get_target_bit(large_hash, i);
+			target_bit = get_target_bit2(large_hash, i);
 			if (! filter.get(target_bit) ) {
 				return false;
 			}
