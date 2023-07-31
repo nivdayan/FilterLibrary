ae96e313e1664d21952726cb6cba6ded42b125c1
“Niv <nivdayan@gmail.com>
2023-07-31 10:23:01 +0200

    deleting output files, and larger experiments

--- run_exps.sh
+++ run_exps.sh
@@ -3,20 +3,23 @@
 
 javac filters/*.java bitmap_implementations/*.java infiniFilter_experiments/*.java  
 
-java infiniFilter_experiments.Experiment1 16 12 24 exp1
-java infiniFilter_experiments.Experiment1 8 12 24 exp2
+rm figure13.png figure14.png figure15.png figure16.png
+rm -rf exp*
+
+java infiniFilter_experiments.Experiment1 16 12 31 exp1
+java infiniFilter_experiments.Experiment1 8 12 31 exp2
 gnuplot figure13_script.gnuplot  
 mv output.png figure13.png 
 
-java infiniFilter_experiments.Experiment2 16 10 24 exp3
-java infiniFilter_experiments.Experiment2 8 10 24 exp4
+java infiniFilter_experiments.Experiment2 16 10 31 exp3
+java infiniFilter_experiments.Experiment2 8 10 31 exp4
 gnuplot figure14_script.gnuplot 
 mv output.png figure14.png 
 
-java infiniFilter_experiments.Experiment3 8 10 24 exp5
+java infiniFilter_experiments.Experiment3 8 10 31 exp5
 gnuplot figure15_script.gnuplot 
 mv output.png figure15.png 
 
-java infiniFilter_experiments.Experiment4 8 10 24 exp6
+java infiniFilter_experiments.Experiment4 8 10 31 exp6
 gnuplot figure16_script.gnuplot 
 mv output.png figure16.png 
